5ee425a65c312b23bbc43550ef80ce3a02c814cc
Водилов Максим <@corp.itstep>
2020-01-10 17:20:19 +0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Данила, ты что крейзи?");
+            Console.WriteLine("Текст");
+
+            string name = "Данила крейзи";
+            Console.WriteLine($"Меня зовут {name}");
+
+            Console.WriteLine("Тут надо написать своё хобби: ");
+            string hobby = Console.ReadLine();
+            Console.WriteLine($"Моё хобби: {hobby}");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
